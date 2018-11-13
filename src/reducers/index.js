bebd4cf9134a0781d50c1e3b983702acba08ca26
sjlutterbie <simon.lutterbie@ltrbe.com>
2018-11-13 22:04:26 +0000

    Initial state created

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,6 +1,11 @@
 import * as actions from '../actions';
 
-const initialState = {}
+const initialState = {
+  guesses: [],
+  feedback: 'Make your guess!',
+  auralStatus: '',
+  correctAnswer: Math.floor(Math.random() * 100) + 1
+};
 
 export const hotOrColdReducer = (state=initialState, action) => {
   
